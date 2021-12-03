d9d559caf289b0969f38b81f45e38dc1de3040fd
Tr1d3nt <saudagha2@gmail.com>
2021-12-02 18:16:13 -0700

    made propertyController and propertyclass
    added foundation for both. testing setProperty in PropertyController

--- src/controllers/PropertyController.java
+++ src/controllers/PropertyController.java
@@ -1,12 +1,46 @@
+
 package controllers;
 
+import tasks.*;
 import java.sql.*;
 import java.util.*;
 
 public class PropertyController extends DBController {
 
+    private ArrayList<Property> properties = new ArrayList<Property>();
+
     public PropertyController() {
+
         initializeConnection();
+
+    }
+
+    public void setProperty(Property property) {
+
+        try {
+
+            String query = "INSERT INTO property (propertyID, landLordID, address, type, bedrooms, bathrooms, furnished, quadrant, status, submitted, expiry)"
+                    + " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+            PreparedStatement stmt = dbConnect.prepareStatement(query);
+            stmt.setInt(1, property.getPropertyID());
+            stmt.setInt(2, property.getLandlordID());
+            stmt.setString(3, property.getAddress());
+            stmt.setString(4, property.getType());
+            stmt.setInt(5, property.getBedandBath()[0]);
+            stmt.setInt(6, property.getBedandBath()[1]);
+            stmt.setInt(7, property.getFurnished());
+            stmt.setString(8, property.getQuadrant());
+            stmt.setString(9, property.getStatus());
+            stmt.setDate(10, property.getSubmitted());
+            stmt.setDate(11, property.getExpiry());
+
+            stmt.execute();
+            stmt.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -58,8 +92,9 @@ public class PropertyController extends DBController {
     public static void main(String[] args) {
 
         PropertyController p = new PropertyController();
+        Property prop = new Property(0, 0, "123 street", "Apartment", 2, 1, 1, "NW");
 
-        p.getAllProperty();
+        p.setProperty(prop);
 
     }
 
---
+++ src/tasks/Property.java
@@ -0,0 +1,90 @@
+package tasks;
+
+import java.sql.Date;
+import java.util.Calendar;
+
+import controllers.*;
+
+public class Property {
+
+    private int propertyID;
+    private int landLordID;
+    private String address;
+    private String type;
+    private int bedrooms;
+    private int bathrooms;
+    private int furnished;
+    private String quadrant;
+    private String status;
+    private java.sql.Date submitted;
+    private java.sql.Date expiry;
+    private PropertyController propertyController;
+
+    // constructor
+    public Property(int propertyID, int landLordID, String address, String type, int bed, int bath,
+            int furnished, String quadrant) {
+
+        this.propertyID = propertyID;
+        this.landLordID = landLordID;
+        this.address = address;
+        this.type = type;
+        this.bedrooms = bed;
+        this.bathrooms = bath;
+        this.furnished = furnished;
+        this.quadrant = quadrant;
+        this.status = "Active";
+
+        Calendar calendar = Calendar.getInstance();
+        this.submitted = new java.sql.Date(System.currentTimeMillis());
+        this.expiry = new java.sql.Date(System.currentTimeMillis());
+
+    }
+
+    // getters
+    public int getPropertyID() {
+        return this.propertyID;
+    }
+
+    public int getLandlordID() {
+        return this.landLordID;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public int[] getBedandBath() {
+
+        int[] temp = { this.bedrooms, this.bathrooms };
+
+        return temp;
+
+    }
+
+    public int getFurnished() {
+        return this.furnished;
+    }
+
+    public String getQuadrant() {
+
+        return this.quadrant;
+
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public Date getSubmitted() {
+        return this.submitted;
+    }
+
+    public Date getExpiry() {
+        return this.expiry;
+    }
+
+}
