c7816a5e3de12ad8e263f4651cb2aa76072c3a3e
jcacierto <78886859+jcacierto@users.noreply.github.com>
2021-12-05 00:22:57 -0700

    Added PropertyListings

---
+++ src/tasks/PropertyListings.java
@@ -0,0 +1,13 @@
+package tasks;
+
+
+public class PropertyListings {
+    private ArrayList<Observer> observers;
+    private PropertyController propertyController;
+    private Strategy strategy;
+
+    public void checkPropertyController(){
+        System.out.println("checkPropertyController method called.");
+    }
+    
+}
