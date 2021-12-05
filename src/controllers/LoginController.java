d4a68c26d076ddec6780e2b6b84b7a5323ce796a
jcacierto <78886859+jcacierto@users.noreply.github.com>
2021-12-05 00:28:05 -0700

    Added LoginController class

---
+++ src/controllers/LoginController.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import entity.*;
+
+public class LoginController {
+    public void addUser(){
+        System.out.println("addUser method called.");
+    }
+
+    public boolean verifyUser(User user){
+        return true;
+    }
+}
