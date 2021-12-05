e56555df5eefe08346946a7257d0c3ed994875f9
jcacierto <78886859+jcacierto@users.noreply.github.com>
2021-12-05 00:07:29 -0700

    edited Fee class

--- src/tasks/Fee.java
+++ src/tasks/Fee.java
@@ -1,7 +1,20 @@
 package tasks;
 
-
+import controllers.*;
 
 public class Fee{
-    
+    private double fee;
+    private String startDate;
+    private String endDate;
+    private boolean isPaid;
+    private FeeController feeController; //don't forget to define this
+
+    public void payFee(double fee, int period){
+        System.out.println("payFee method called.");
+    }
+
+    public void setFee(double fee, int period){
+        System.out.println("setFee method called.");
+    }
+
 }
