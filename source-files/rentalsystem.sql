76fd40ae4d098a5aa291579a8491fa45047dc8de
Tr1d3nt <34665547+Tr1d3nt@users.noreply.github.com>
2021-12-04 22:29:54 -0700

    updated rentalsystems.sql

--- source-files/rentalsystem.sql
+++ source-files/rentalsystem.sql
@@ -8,22 +8,22 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- Schema mydb
 -- -----------------------------------------------------
 -- -----------------------------------------------------
--- Schema RENTALSYSTEM
+-- Schema rentalsystem
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `RENTALSYSTEM` ;
+DROP SCHEMA IF EXISTS `rentalsystem` ;
 
 -- -----------------------------------------------------
--- Schema RENTALSYSTEM
+-- Schema rentalsystem
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `RENTALSYSTEM` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE `RENTALSYSTEM` ;
+CREATE SCHEMA IF NOT EXISTS `rentalsystem` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `rentalsystem` ;
 
 -- -----------------------------------------------------
--- Table `RENTALSYSTEM`.`fee`
+-- Table `rentalsystem`.`fee`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `RENTALSYSTEM`.`fee` ;
+DROP TABLE IF EXISTS `rentalsystem`.`fee` ;
 
-CREATE TABLE IF NOT EXISTS `RENTALSYSTEM`.`fee` (
+CREATE TABLE IF NOT EXISTS `rentalsystem`.`fee` (
   `managerID` INT NOT NULL,
   `amount` INT NOT NULL,
   `period` INT NOT NULL,
@@ -34,11 +34,11 @@ COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `RENTALSYSTEM`.`notifications`
+-- Table `rentalsystem`.`notifications`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `RENTALSYSTEM`.`notifications` ;
+DROP TABLE IF EXISTS `rentalsystem`.`notifications` ;
 
-CREATE TABLE IF NOT EXISTS `RENTALSYSTEM`.`notifications` (
+CREATE TABLE IF NOT EXISTS `rentalsystem`.`notifications` (
   `renterID` INT NOT NULL,
   `type` VARCHAR(45) NULL DEFAULT NULL,
   `bedrooms` INT NULL DEFAULT NULL,
@@ -52,11 +52,11 @@ COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `RENTALSYSTEM`.`property`
+-- Table `rentalsystem`.`property`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `RENTALSYSTEM`.`property` ;
+DROP TABLE IF EXISTS `rentalsystem`.`property` ;
 
-CREATE TABLE IF NOT EXISTS `RENTALSYSTEM`.`property` (
+CREATE TABLE IF NOT EXISTS `rentalsystem`.`property` (
   `propertyID` INT NOT NULL,
   `landLordID` INT NULL DEFAULT NULL,
   `address` VARCHAR(300) NULL DEFAULT NULL,
@@ -66,8 +66,8 @@ CREATE TABLE IF NOT EXISTS `RENTALSYSTEM`.`property` (
   `furnished` INT NULL DEFAULT NULL,
   `quadrant` VARCHAR(2) NULL DEFAULT NULL,
   `status` VARCHAR(45) NULL DEFAULT NULL,
-  `submitted` VARCHAR(45) NULL DEFAULT NULL,
-  `expiry` VARCHAR(45) NULL DEFAULT NULL,
+  `submitted` DATE NULL DEFAULT NULL,
+  `expiry` DATE NULL DEFAULT NULL,
   PRIMARY KEY (`propertyID`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
@@ -75,11 +75,11 @@ COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `RENTALSYSTEM`.`user`
+-- Table `rentalsystem`.`user`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `RENTALSYSTEM`.`user` ;
+DROP TABLE IF EXISTS `rentalsystem`.`user` ;
 
-CREATE TABLE IF NOT EXISTS `RENTALSYSTEM`.`user` (
+CREATE TABLE IF NOT EXISTS `rentalsystem`.`user` (
   `userID` INT NOT NULL,
   `Email` VARCHAR(100) NULL DEFAULT NULL,
   `Password` VARCHAR(45) NULL DEFAULT NULL,
