0a9f9c4059a03c9919c57e876822a713820c86a1
jcacierto <78886859+jcacierto@users.noreply.github.com>
2021-12-04 21:48:15 -0700

    Added Login class

---
+++ src/tasks/Login.java
@@ -0,0 +1,11 @@
+package tasks;
+
+
+public class Login{
+
+    private static Login onlyInstance;
+
+    public Login getOnlyInstance(){
+        System.out.println("getOnlyInstance method called.");
+        return onlyInstance;
+}
