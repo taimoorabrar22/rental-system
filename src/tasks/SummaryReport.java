d388c3d797ef29124eddf4549bce71984a807e96
jcacierto <78886859+jcacierto@users.noreply.github.com>
2021-12-04 21:34:34 -0700

    added SummaryReport class

---
+++ src/tasks/SummaryReport.java
@@ -0,0 +1,13 @@
+package tasks;
+
+import controllers.*;
+
+public class SummaryReport{
+
+    private PropertyController propertyController;
+
+    public String getSummaryReport(int beginDate, int endDate){
+        return "getSummaryReport function called.";
+    }
+
+}
